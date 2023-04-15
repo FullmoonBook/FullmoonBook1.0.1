0dc52a7770789dd2db0265216963f93ea074becb
kangseoju <kangseoju@naver.com>
2023-04-15 12:12:21 +0900

    반복문 수정

--- fullmoonbook/src/fullmoonbook/common/FrontController.java
+++ fullmoonbook/src/fullmoonbook/common/FrontController.java
@@ -51,6 +51,8 @@ public class FrontController {
 	private Scanner scanner = new Scanner(System.in);
 	public boolean loginRun = true;
 	public boolean mainRun = true;
+	public boolean nowRun = true;
+
 	int login = 0;
 	int goal = 0;
 	MemberVO vo = null;
@@ -81,18 +83,15 @@ public class FrontController {
 				}
 			case 3: // 프로그램 종료 빠져나가기.
 				loginRun = false;
+			
 				break;
 			}
 			while (mainRun) {
 				mainView.welcome();
-				
 				int menu = mainView.mainMenu(scanner);
 				switch (menu) {
-				case 1://현재 챌린지 
-					
-					int menu2 = mainView.nowBookMenu(scanner);
-					switch (menu2) {
-					case 1: //현재 챌린지 정보
+				case 1:// 이번 보름 챌린지
+					while (nowRun) {
 						BookVO nowBook = bookController.getNowChallenge("0003");
 						bookView.getNowChallenge(nowBook);
 						ChallengeDAO dao = ChallengeDAO.getInstance();
@@ -100,7 +99,6 @@ public class FrontController {
 						System.out.println("\t    챌린저: " + challenger + "명");
 						System.out.println();
 						goal = challController.getGoal(session);
-						System.out.println(goal); //로그
 						if (goal == 100) {
 							System.out.println("         달성률: ■■■■■ " + goal + " %");
 						} else if (goal >= 80) {
@@ -118,9 +116,9 @@ public class FrontController {
 						} else {
 							System.out.println("         달성률: □□□□□ " + goal + " %");
 						}
-						
-						int menu3 = mainView.startChallengeMenu(scanner);
-						switch (menu3) {
+
+						int menu2 = mainView.startChallengeMenu(scanner);
+						switch (menu2) {
 						case 1: // 챌린지 시작
 							try {
 								ChallengeVO iMember = challengeView.insertChallengeStatus(scanner);
@@ -129,57 +127,38 @@ public class FrontController {
 									challengeView.insertStatusResult(insertStatus);
 								} else {
 									System.out.println("현재 " + challenger + "명의 챌린저가 도전 중입니다. 함께해 주세요");
-
+									continue;
 								}
 
 							} catch (Exception e) {
 								System.out.println("이미 진행 중입니다.");
+								continue;
 							}
-							break;
-
 						case 2: // 페이지 입력
-							System.out.println(challController.getStatus(session)); //로그
 							if (challController.getStatus(session).equals("y")) {
 								ChallengeVO iPage = challengeView.updateGoal(scanner);
 								int updateGoal = challController.updateGoal(iPage);
 								challengeView.updateGoalResult(updateGoal);
+								continue;
+
 							} else {
 								System.out.println("먼저 챌린지를 시작해 주세요.");
-								break;
+								continue;
+
 							}
+
+						case 3: // 메인 페이지로
+							nowRun = false;
 							break;
+							
+						default:
+							continue;
+
 						}
-
-//						if (menu3 == 1) {
-//							break;
-//						}
 						break;
-
-					case 2: // 도서 리뷰
-						if (goal == 100) {
-							List<ReviewVO> reivews = reviewController.getReviews();
-							reviewView.getReviews(reivews);
-							ReviewVO iReview = reviewView.inputReview(scanner);
-							if (iReview != null) {
-								int insertReview = reviewController.insertReview(iReview);
-								reviewView.inputResult(insertReview);
-							} else {
-								System.out.println("완독하고 오세요...");
-							}
-							int menu4 = mainView.reviewMenu(scanner);
-							if (menu4 == 2) {
-								break;
-							} else if (BookApplication.challengeGetSession().getGoal() == 100) {
-							} else {
-								System.out.println("달성률 채우기");
-								break;
-							}
-						}
-					case 3:
-						break;
-
 					}
 					break;
+
 				case 2: // 다음 챌린지
 					BookVO nextBook = bookController.getNextChallenge("0004");
 					bookView.getNextChallenge(nextBook);
@@ -187,26 +166,42 @@ public class FrontController {
 					if (menu5 == 1) {
 						break;
 					}
-					break;
-				case 3: // 마이페이지
-					System.out.println();
-					int menu6 = mainView.toMainMenu(scanner);
-					if (menu6 == 1) {
-						break;
+
+				case 3: // 후기
+					if (goal == 100) {
+						List<ReviewVO> reivews = reviewController.getReviews();
+						reviewView.getReviews(reivews);
+						ReviewVO iReview = reviewView.inputReview(scanner);
+						if (iReview != null) {
+							int insertReview = reviewController.insertReview(iReview);
+							reviewView.inputResult(insertReview);
+						} else {
+							System.out.println("완독하고 오세요...");
+						}
+						int menu4 = mainView.reviewMenu(scanner);
+						if (menu4 == 2) {
+							break;
+						} else if (BookApplication.challengeGetSession().getGoal() == 100) {
+						} else {
+							System.out.println("달성률 채우기");
+							break;
+						}
 					}
-					break;
-				case 4: // 로그아웃
+
+				
+				case 4: // 마이페이지
 					System.out.println("로그아웃 되었습니다");
 					mainRun = false;
-					break;
+					continue;
 
 				default:
-					break;
+					continue;
 				}
 			}
 		}
 	}
 }
+
 //	//	joinController.join(scanner);
 //
 //
